a4c90e6e1612b035e7c40ce15625bdf92f274756
Peter Pesotskiy <ppeter777@yandex.ru>
2024-12-05 23:15:46 +0300

    linter corr#12

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -3,16 +3,11 @@ package hexlet.code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import io.sentry.Sentry;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@RestController
-@RequestMapping("/api")
 public class AppApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
