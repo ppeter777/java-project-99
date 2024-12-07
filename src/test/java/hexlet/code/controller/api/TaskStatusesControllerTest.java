a82d50a596086c08cf71ceae3db4d8a2b4dbc13e
Peter Pesotskiy <ppeter777@yandex.ru>
2024-12-08 00:07:57 +0300

    linter corr

--- src/test/java/hexlet/code/controller/api/TaskStatusesControllerTest.java
+++ src/test/java/hexlet/code/controller/api/TaskStatusesControllerTest.java
@@ -13,7 +13,7 @@ import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.UserRepository;
 import hexlet.code.util.ModelGenerator;
 import org.instancio.Instancio;
-import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
