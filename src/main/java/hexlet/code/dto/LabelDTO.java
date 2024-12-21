419de6ee555a2f83c269c1a9dd2bef660b5f928d
Peter Pesotskiy <ppeter777@yandex.ru>
2024-12-22 00:43:09 +0300

    linter corr

--- src/main/java/hexlet/code/dto/LabelDTO.java
+++ src/main/java/hexlet/code/dto/LabelDTO.java
@@ -1,12 +1,10 @@
 package hexlet.code.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import hexlet.code.model.Task;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDate;
-import java.util.Set;
 
 @Getter
 @Setter
--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -1,6 +1,13 @@
 package hexlet.code.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -9,7 +16,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDate;
 import java.util.Set;
-
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
