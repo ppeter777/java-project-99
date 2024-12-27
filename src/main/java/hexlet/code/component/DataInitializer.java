512974f9761ed727fbc8d2a859ce0305603481ba
Peter Pesotskiy <ppeter777@yandex.ru>
2024-12-27 11:22:31 +0300

    modify Datainitializer temp

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -40,35 +40,35 @@ public class DataInitializer implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        var email = "hexlet@example.com";
-        if (userRepository.findByEmail(email).isEmpty()) {
-            var userData = new User();
-            userData.setEmail(email);
-            userData.setPasswordDigest("qwerty");
-            userService.createUser(userData);
-        }
-
-        List<TaskStatusCreateDTO> initStatuses = new ArrayList<>(List.of(
-            new TaskStatusCreateDTO("Draft", "draft"),
-            new TaskStatusCreateDTO("To review", "to_review"),
-            new TaskStatusCreateDTO("To be fixed", "to_be_fixed"),
-            new TaskStatusCreateDTO("To publish", "to_publish"),
-            new TaskStatusCreateDTO("Published", "published")));
-
-        for (TaskStatusCreateDTO status : initStatuses) {
-            if (taskStatusRepository.getTaskStatusByName(status.getName()).isEmpty()) {
-                taskStatusRepository.save(taskStatusMapper.map(status));
-            }
-        }
-
-        List<LabelCreateDTO> initLabels = new ArrayList<>(List.of(
-                new LabelCreateDTO("bug"),
-                new LabelCreateDTO("feature")));
-
-        for (LabelCreateDTO label : initLabels) {
-            if (labelRepository.getLabelByName(label.getName()).isEmpty()) {
-                labelRepository.save(labelMapper.map(label));
-            }
-        }
+//        var email = "hexlet@example.com";
+//        if (userRepository.findByEmail(email).isEmpty()) {
+//            var userData = new User();
+//            userData.setEmail(email);
+//            userData.setPasswordDigest("qwerty");
+//            userService.createUser(userData);
+//        }
+//
+//        List<TaskStatusCreateDTO> initStatuses = new ArrayList<>(List.of(
+//            new TaskStatusCreateDTO("Draft", "draft"),
+//            new TaskStatusCreateDTO("To review", "to_review"),
+//            new TaskStatusCreateDTO("To be fixed", "to_be_fixed"),
+//            new TaskStatusCreateDTO("To publish", "to_publish"),
+//            new TaskStatusCreateDTO("Published", "published")));
+//
+//        for (TaskStatusCreateDTO status : initStatuses) {
+//            if (taskStatusRepository.getTaskStatusByName(status.getName()).isEmpty()) {
+//                taskStatusRepository.save(taskStatusMapper.map(status));
+//            }
+//        }
+//
+//        List<LabelCreateDTO> initLabels = new ArrayList<>(List.of(
+//                new LabelCreateDTO("bug"),
+//                new LabelCreateDTO("feature")));
+//
+//        for (LabelCreateDTO label : initLabels) {
+//            if (labelRepository.getLabelByName(label.getName()).isEmpty()) {
+//                labelRepository.save(labelMapper.map(label));
+//            }
+//        }
     }
 }
